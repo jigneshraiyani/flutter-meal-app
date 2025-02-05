83c6c5d8cbe78fd8782b384d92db3b4667b5cdc4
Jignesh Raiyani <jignesh.raiyani@Jigneshs-MacBook-Air.local>
2025-02-05 07:18:39 +0530

    Added tap gesture to category

--- meal_app/lib/screens/category_screen.dart
+++ meal_app/lib/screens/category_screen.dart
@@ -16,7 +16,7 @@ class CategoryScreen extends StatelessWidget {
         padding: EdgeInsets.all(20),
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
-          childAspectRatio: 2/3,
+          childAspectRatio: 3/2,
           crossAxisSpacing: 20,
           mainAxisSpacing: 20,
           ),
